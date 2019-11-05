2e31d6bc3ca890d917d9de03d0217f94ac098f57
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-05 15:04:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
   array_of_words = string.split(" ")
-    backwards_array = array_of_words.map do |word|
+    backwards_array = array_of_words.collect do |word|
       word = word.reverse
     end
   string2 = backwards_array.join(" ")
-  p string2
+  string2
 end
 
 reverse_each_word("Hello there, world")
