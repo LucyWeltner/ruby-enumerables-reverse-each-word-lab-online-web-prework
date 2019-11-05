11a283b20cd692d0f60002dcd56357e520f598fc
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-05 14:44:05 +0000

    Automatically backed up by Learn

--- reverse.rb
+++ reverse.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
-  array_of_characters = string.split("")
-  p array_of_characters
+  array_of_characters = string.split(" ")
+  array_of_characters
 end
 
 reverse_each_word("Hello there, and how are you?")
