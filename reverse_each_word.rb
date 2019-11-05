9baed4f3fca25c66436edda3c49fd7e0bd4dc388
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-05 14:44:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
   array_of_characters = string.split(" ")
-  p array_of_characters
+  array_of_characters
 end
+
+p reverse_each_word("Hello there, and how are you?")
   #array_of_characters.each do |char|
   # new_array[0] = new_array
