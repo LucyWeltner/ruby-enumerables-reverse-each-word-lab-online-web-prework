6a87bde4cec1b9226b8f42abe3726bdd3c46fc2c
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-05 14:49:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array_of_words = string.split(" ")
   string2 = 0
   array_of_words.each do |word|
-    string2 += word.reverse
+    string2 = string2 + word.reverse
     string2
   end
 end
